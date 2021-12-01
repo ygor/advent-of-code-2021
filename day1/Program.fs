ce8738d50868c3f71ba1efc7336ec1a136eca393
Ygor Geurts <ygor.geurts@gmail.com>
2021-12-01 22:38:18 +0100

    simplify and .net6

--- day1/Program.fs
+++ day1/Program.fs
@@ -2,10 +2,10 @@
 
 let measurements = File.ReadAllLines("input.txt") |> Seq.toList |> List.map int
 let part1 = List.pairwise >> List.filter (fun (x, y) -> y > x) >> List.length
-let part2 = measurements |> List.windowed 3 |> List.map List.sum |> part1
+let part2 = List.windowed 3 >> List.map List.sum >> part1
 
 [<EntryPoint>]
 let main _ =
     printfn $"Part 1: {part1 measurements}"
-    printfn $"Part 2: {part2}"
+    printfn $"Part 2: {part2 measurements}"
     0
