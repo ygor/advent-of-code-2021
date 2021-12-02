f4863ffffd248dae59fb64245d14b8f956713a72
Ygor Geurts <ygor.geurts@gmail.com>
2021-12-02 16:21:46 +0100

    formatting

--- day02/Program.fs
+++ day02/Program.fs
@@ -6,9 +6,9 @@ let course =
     |> Seq.fold
         (fun ((h1, d1), (h2, d2, aim)) line ->
             match line with
-            | Regex "forward (\d+)" [ x ] -> ((h1 + int x, d1), (h2 + int x, d2 + (aim * int x), aim))
-            | Regex "down (\d+)" [ x ] -> ((h1, d1 + int x), (h2, d2, aim + int x))
-            | Regex "up (\d+)" [ x ] -> ((h1, d1 - int x), (h2, d2, aim - int x))
+            | Regex "forward (\d+)" [ x ] -> (h1 + int x, d1), (h2 + int x, d2 + (aim * int x), aim)
+            | Regex "down (\d+)" [ x ] -> (h1, d1 + int x), (h2, d2, aim + int x)
+            | Regex "up (\d+)" [ x ] -> (h1, d1 - int x), (h2, d2, aim - int x)
             | _ -> failwithf $"Invalid input %s{line}")
         ((0, 0), (0, 0, 0))
 
