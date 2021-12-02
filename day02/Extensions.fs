d914c3492dc383e368050b3380f2dc4ceac8b763
Ygor Geurts <ygor.geurts@gmail.com>
2021-12-02 09:17:56 +0100

    day2

---
+++ day02/Extensions.fs
@@ -0,0 +1,9 @@
+module Extensions
+
+open System.Text.RegularExpressions
+
+let (|Regex|_|) pattern input =
+    let m = Regex.Match(input, pattern)
+    if m.Success
+    then Some(List.tail [ for g in m.Groups -> g.Value ])
+    else None
---
+++ day02/Program.fs
@@ -0,0 +1,30 @@
+﻿open System.IO
+open Extensions
+
+let input = File.ReadAllLines("input.txt") 
+
+let part1 =
+    input
+    |> Seq.fold (fun (horizontal, depth) line ->
+        match line with
+        | Regex "forward (\d+)" [ number ] -> (horizontal + int number, depth)
+        | Regex "down (\d+)" [ number ] -> (horizontal, depth + int number)
+        | Regex "up (\d+)" [ number ] -> (horizontal, depth - int number)
+        | _ -> failwithf $"Invalid input %s{line}") (0, 0)
+    |> (fun (a, b) -> a * b)
+
+let part2 =
+    input
+    |> Seq.fold (fun (horizontal, depth, aim) line ->
+        match line with
+        | Regex "forward (\d+)" [ number ] -> (horizontal + int number, depth + (aim * int number), aim)
+        | Regex "down (\d+)" [ number ] -> (horizontal, depth, aim + int number)
+        | Regex "up (\d+)" [ number ] -> (horizontal, depth, aim - int number)
+        | _ -> failwithf $"Invalid input %s{line}") (0, 0, 0)
+    |> (fun (a, b, _) -> a * b)
+
+[<EntryPoint>]
+let main _ =
+    printfn $"Part 1: {part1}"
+    printfn $"Part 2: {part2}"
+    0
