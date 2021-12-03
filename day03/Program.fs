ce06507f6f6b293fe3e0206b0b01e76a50bf761c
Ygor Geurts <ygor.geurts@gmail.com>
2021-12-03 10:06:20 +0100

    day 3 part1

---
+++ day03/Program.fs
@@ -0,0 +1,25 @@
+﻿open System
+open System.IO
+open MathNet.Numerics.LinearAlgebra
+
+let report =
+    File.ReadAllLines("input.txt")
+    |> Seq.map (Seq.map (string >> float))
+    |> matrix
+    |> Matrix.toColSeq
+
+let commonBits (report: Vector<float> seq) predicate =
+    report
+    |> Seq.map (fun col ->
+        if (predicate (Vector.sum col) (float (Vector.length col) / 2.0)) then "1" else "0")
+    |> Seq.reduce (+)
+
+let part1 =
+    let gamma = commonBits report (>)
+    let epsilon = commonBits report (<)
+    Convert.ToInt32(gamma, 2) * Convert.ToInt32(epsilon, 2)
+    
+[<EntryPoint>]
+let main _ =
+    printfn $"Part 1: %i{part1}"
+    0
