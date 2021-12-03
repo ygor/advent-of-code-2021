ade22d6f4c352d0e915c3bcaf2be4a17a3d42644
Ygor Geurts <ygor.geurts@gmail.com>
2021-12-03 12:23:59 +0100

    formatting

--- day03/Program.fs
+++ day03/Program.fs
@@ -20,39 +20,32 @@ let leastCommonValue (column: float seq) =
     commonValue column Seq.minBy
 
 let commonBits valueFn (report: Matrix<float>) =
-    let value =
-        report
-        |> Matrix.toColSeq
-        |> Seq.map (valueFn >> string)
-        |> Seq.reduce (+)
-    
-    Convert.ToInt32(value, 2)
+    report
+    |> Matrix.toColSeq
+    |> Seq.map (valueFn >> string)
+    |> Seq.reduce (+)
+    |> (fun value -> Convert.ToInt32(value, 2))
 
-let gamma =
-    commonBits mostCommonValue
-
-let epsilon =
-    commonBits leastCommonValue
+let powerConsumption =
+    commonBits mostCommonValue report * commonBits leastCommonValue report 
 
 let rate valueSelector (report: Matrix<float>) =
-    let value =
-        report
-        |> Matrix.toColSeqi 
-        |> Seq.fold (fun (report': Matrix<float>) (i, _) ->
-            if report'.RowCount = 1 then report'
-            else
-                let column = report'.Column(i)
-                let most, least = mostCommonValue column, leastCommonValue column                
-                report'
-                |> Matrix.toRowSeq
-                |> Seq.filter (fun row -> row.At(i) = valueSelector most least)
-                |> matrix) report
-        |> Matrix.toRowSeq
-        |> Seq.head
-        |> Seq.map string
-        |> Seq.reduce (+)
-        
-    Convert.ToInt32(value, 2)
+    report
+    |> Matrix.toColSeqi 
+    |> Seq.fold (fun (report': Matrix<float>) (i, _) ->
+        if report'.RowCount = 1 then report'
+        else
+            let column = report'.Column(i)
+            let most, least = mostCommonValue column, leastCommonValue column                
+            report'
+            |> Matrix.toRowSeq
+            |> Seq.filter (fun row -> row.At(i) = valueSelector most least)
+            |> matrix) report
+    |> Matrix.toRowSeq
+    |> Seq.head
+    |> Seq.map string
+    |> Seq.reduce (+)
+    |> (fun value -> Convert.ToInt32(value, 2))
 
 let oxygen =
     rate (fun most least -> if most = least then 1.0 else most)
@@ -62,6 +55,6 @@ let scrubbing =
     
 [<EntryPoint>]
 let main _ =
-    printfn $"Part 1: %i{gamma report * epsilon report}"
+    printfn $"Part 1: %i{powerConsumption}"
     printfn $"Part 2: %i{scrubbing report * oxygen report}"
     0
