62f72fc9cef630cba60fb257bf95bfacbad03c33
Ygor Geurts <ygor.geurts@gmail.com>
2021-12-04 14:44:29 +0100

    simplify

--- day04/Extensions.fs
+++ day04/Extensions.fs
@@ -1,11 +1,17 @@
 module day04.Extensions
 
+open System.Text.RegularExpressions
+
 module String =
-    let split (sep: string) (value: string) = value.Split(sep) |> List.ofArray
+    let split (sep: string) (input: string) = input.Split(sep) |> List.ofArray
+    
+    let trim (input: string) =
+        input.Trim()
+        
+module Regex =
+    let split (pattern: string) (input: string) =
+        Regex.Split(input, pattern) |> List.ofArray
 
 module Seq =
     let any predicate seq =
-        seq
-        |> Seq.filter predicate
-        |> Seq.length
-        |> (<) 0
+        seq |> Seq.filter predicate |> Seq.length |> (<) 0
