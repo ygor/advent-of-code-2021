8f0913ca60c9bfbb90725fba74389335bec76733
Ygor Geurts <ygor.geurts@gmail.com>
2021-12-05 11:13:19 +0100

    formatting

--- day05/Extensions.fs
+++ day05/Extensions.fs
@@ -4,6 +4,13 @@ open System.Text.RegularExpressions
 
 let (|Regex|_|) pattern input =
     let m = Regex.Match(input, pattern)
-    if m.Success
-    then Some(List.tail [ for g in m.Groups -> g.Value ])
-    else None
+
+    if m.Success then
+        Some(List.tail [ for g in m.Groups -> g.Value ])
+    else
+        None
+
+module Math =
+    let sign value =
+        if value >= 0 then 1
+        else -1
--- day05/Program.fs
+++ day05/Program.fs
@@ -13,15 +13,15 @@ let horizontals =
     lines
     |> List.filter (fun ((x1, y1), (x2, y2)) -> x1 = x2 || y1 = y2)
     |> List.map (fun ((x1, y1), (x2, y2)) ->
-        List.allPairs [x1 .. (if x2 > x1 then 1 else -1) .. x2] [y1 .. (if y2 > y1 then 1 else -1) .. y2])
+        List.allPairs [x1 .. Math.sign (x2 - x1) .. x2] [y1 .. Math.sign (y2 - y1) .. y2])
 
 let verticals =
     lines
     |> List.filter (fun ((x1, y1), (x2, y2)) -> abs (x2 - x1) = abs (y2 - y1))
     |> List.map (fun ((x1, y1), (x2, y2)) ->
-        List.zip [int x1 .. (if x2 > x1 then 1 else -1) .. int x2] [int y1 .. (if y2 > y1 then 1 else -1) .. int y2])
+        List.zip [int x1 .. Math.sign (x2 - x1) .. int x2] [int y1 .. Math.sign (y2 - y1) .. int y2])
     
-let overlap lines =
+let overlaps lines =
     lines
     |> List.concat
     |> List.groupBy id
@@ -30,6 +30,6 @@ let overlap lines =
     
 [<EntryPoint>]
 let main _ =
-    printfn $"Part 1: {overlap horizontals}"
-    printfn $"Part 2: {overlap (horizontals @ verticals)}"
+    printfn $"Part 1: {overlaps horizontals}"
+    printfn $"Part 2: {overlaps (horizontals @ verticals)}"
     0
