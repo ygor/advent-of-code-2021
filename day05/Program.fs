6ef7423944ef65b7022bca90d2a3c55ab246426b
Ygor Geurts <ygor.geurts@gmail.com>
2021-12-05 08:14:32 +0100

    formatting

--- day05/Program.fs
+++ day05/Program.fs
@@ -3,11 +3,10 @@ open day05.Extensions
 
 let lines =
     File.ReadAllLines("input.txt")
-    |> Seq.map
-        (fun line ->
-            match line with
-            | Regex "(\d+),(\d+) -> (\d+),(\d+)" [ x1; y1; x2; y2 ] -> (int x1, int y1), (int x2, int y2)
-            | _ -> failwithf $"Invalid input %s{line}")
+    |> Seq.map (fun line ->
+        match line with
+        | Regex "(\d+),(\d+) -> (\d+),(\d+)" [ x1; y1; x2; y2 ] -> (int x1, int y1), (int x2, int y2)
+        | _ -> failwithf $"Invalid input %s{line}")
     |> List.ofSeq
 
 let horizontals =
@@ -19,9 +18,10 @@ let horizontals =
 let verticals =
     lines
     |> List.filter (fun ((x1, y1), (x2, y2)) -> abs (x2 - x1) = abs (y2 - y1))
-    |> List.map (fun ((x1, y1), (x2, y2)) -> List.zip [int x1 .. (if x2 > x1 then 1 else -1) .. int x2] [int y1 .. (if y2 > y1 then 1 else -1) .. int y2])
+    |> List.map (fun ((x1, y1), (x2, y2)) ->
+        List.zip [int x1 .. (if x2 > x1 then 1 else -1) .. int x2] [int y1 .. (if y2 > y1 then 1 else -1) .. int y2])
     
-let overlaps lines =
+let overlap lines =
     lines
     |> List.concat
     |> List.groupBy id
@@ -30,6 +30,6 @@ let overlaps lines =
     
 [<EntryPoint>]
 let main _ =
-    printfn $"Part 1: {overlaps horizontals}"
-    printfn $"Part 2: {overlaps (horizontals @ verticals)}"
+    printfn $"Part 1: {overlap horizontals}"
+    printfn $"Part 2: {overlap (horizontals @ verticals)}"
     0
