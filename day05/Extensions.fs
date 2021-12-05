4128899f01d637a4852239dc16c183925a3050f7
Ygor Geurts <ygor.geurts@gmail.com>
2021-12-05 16:19:11 +0100

    simplify

--- day05/Extensions.fs
+++ day05/Extensions.fs
@@ -4,7 +4,7 @@ open System.Text.RegularExpressions
 
 let (..) a b =
     if a < b then seq { a .. b }
-             else seq { a .. -1 .. b }
+    else seq { a .. -1 .. b }
 
 let (|Regex|_|) pattern input =
     let m = Regex.Match(input, pattern)
