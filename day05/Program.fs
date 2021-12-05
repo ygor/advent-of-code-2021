39bcf8d9426c2020f75ddbeb80fe3f7ccf680b2b
Ygor Geurts <ygor.geurts@gmail.com>
2021-12-05 11:18:06 +0100

    simplify

--- day05/Program.fs
+++ day05/Program.fs
@@ -7,29 +7,28 @@ let lines =
         match line with
         | Regex "(\d+),(\d+) -> (\d+),(\d+)" [ x1; y1; x2; y2 ] -> (int x1, int y1), (int x2, int y2)
         | _ -> failwithf $"Invalid input %s{line}")
-    |> List.ofSeq
 
 let horizontals =
     lines
-    |> List.filter (fun ((x1, y1), (x2, y2)) -> x1 = x2 || y1 = y2)
-    |> List.map (fun ((x1, y1), (x2, y2)) ->
-        List.allPairs [x1 .. Math.sign (x2 - x1) .. x2] [y1 .. Math.sign (y2 - y1) .. y2])
+    |> Seq.filter (fun ((x1, y1), (x2, y2)) -> x1 = x2 || y1 = y2)
+    |> Seq.map (fun ((x1, y1), (x2, y2)) ->
+        Seq.allPairs [x1 .. Math.sign (x2 - x1) .. x2] [y1 .. Math.sign (y2 - y1) .. y2])
 
 let verticals =
     lines
-    |> List.filter (fun ((x1, y1), (x2, y2)) -> abs (x2 - x1) = abs (y2 - y1))
-    |> List.map (fun ((x1, y1), (x2, y2)) ->
-        List.zip [x1 .. Math.sign (x2 - x1) .. x2] [y1 .. Math.sign (y2 - y1) .. y2])
+    |> Seq.filter (fun ((x1, y1), (x2, y2)) -> abs (x2 - x1) = abs (y2 - y1))
+    |> Seq.map (fun ((x1, y1), (x2, y2)) ->
+        Seq.zip [x1 .. Math.sign (x2 - x1) .. x2] [y1 .. Math.sign (y2 - y1) .. y2])
     
 let overlaps lines =
     lines
-    |> List.concat
-    |> List.groupBy id
-    |> List.filter (snd  >> List.length >> (<) 1)
-    |> List.length
+    |> Seq.concat
+    |> Seq.groupBy id
+    |> Seq.filter (snd  >> Seq.length >> (<) 1)
+    |> Seq.length
     
 [<EntryPoint>]
 let main _ =
     printfn $"Part 1: {overlaps horizontals}"
-    printfn $"Part 2: {overlaps (horizontals @ verticals)}"
+    printfn $"Part 2: {overlaps (Seq.append horizontals verticals)}"
     0
