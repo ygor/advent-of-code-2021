d88215416473c4508ffae19f277c7c97e38d3dd2
Ygor Geurts <ygor.geurts@gmail.com>
2021-12-06 15:35:48 +0100

    day6 in ruby

--- day06/day06.rb
+++ day06/day06.rb
@@ -1,13 +1,9 @@
 
 numbers = File.open("input.txt").readlines.first.strip.split(",").map(&:to_i)
-fish = numbers.group_by(&:itself).to_h.transform_values(&:size)
+initial_fish = numbers.group_by(&:itself).to_h.transform_values(&:size)
 
-def simulate (fish, days)
-    (1 .. days).inject(fish) { |fish, i| 
-        fish.inject({}) { |fish, (f, count)|  
+puts "#{{:part1 => 80, :part2 => 256 }.transform_values { |days|
+    (1 .. days).inject(initial_fish) { |fish_on_day, i|
+        fish_on_day.inject({}) { |fish, (f, count)|
             if f == 0 then { 6 => count, 8 => count}.inject(fish) { |h, (k, v)| h[k] = h.fetch(k, 0) + v; h }
-            else fish[f - 1] = fish.fetch(f - 1, 0) + count; fish end }}
-end
-
-puts "Part 1: #{simulate(fish, 80).values.sum}"
-puts "Part 2: #{simulate(fish, 256).values.sum}"
+            else fish[f - 1] = fish.fetch(f - 1, 0) + count; fish end }}.values.sum }}"
