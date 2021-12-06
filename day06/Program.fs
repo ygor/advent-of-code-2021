0d9d22809e13ce3ba512fe32649dedd58b03b063
Ygor Geurts <ygor.geurts@gmail.com>
2021-12-06 16:27:47 +0100

    simplify

--- day06/Program.fs
+++ day06/Program.fs
@@ -5,15 +5,14 @@ let fish =
     File.ReadAllText("input.txt")
     |> String.split ","
     |> List.map int
-    |> List.fold (fun (list: bigint list) value ->
-        List.updateAt value (list.[value] + (bigint 1)) list) (List.init 9 (fun _ -> bigint 0)) 
+    |> List.fold (fun list value -> List.updateAt value (list.[value] + (bigint 1)) list) (List.init 9 (fun _ -> bigint 0)) 
 
 let simulate days (initial: List<bigint>) =
     [1 .. days]
     |> Seq.fold (fun fish _ ->
         let fish' = List.tail fish @ [List.head fish]
         List.updateAt 6 (fish'.[6] + fish'.[8]) fish') initial
-    |> List.sum  
+    |> List.sum
     
 [<EntryPoint>]
 let main _ =
