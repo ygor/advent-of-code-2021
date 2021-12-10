6e547cfab68517dfe5ea160c2b6b87f0408d5bb6
Ygor Geurts <ygor.geurts@gmail.com>
2021-12-10 20:51:09 +0100

    simplif

--- day09/Program.fs
+++ day09/Program.fs
@@ -1,36 +1,26 @@
 ﻿open System.IO
 open day09.Extensions
 
-let heightmap =
+let map =
     File.ReadAllLines("input.txt")
-    |> Seq.mapi (fun y line ->
-        line
-        |> Seq.mapi (fun x value -> (x, y), (string >> int) value)
-        |> List.ofSeq)
+    |> Seq.map (fun line -> line |> Seq.map (string >> int) |> List.ofSeq)
     |> List.ofSeq
 
+let height, width = List.length map, List.length map.[0]
+
 let adjacents (x,y) =
-    let height, width = List.length heightmap, List.length heightmap.[0]
-    
     [(-1, 0); (1, 0); (0, -1); (0, 1)]
     |> Seq.map (fun (dx, dy) -> (x + dx, y + dy))
     |> Seq.filter (fun (x, y) -> x >= 0 && x < width && y >= 0 && y < height)
     |> Set.ofSeq
 
 let lows =
-    let height, width = List.length heightmap, List.length heightmap.[0]
-    
-    [0 .. (height - 1)]
-    |> List.fold (fun lows y ->
-        [0 ..  (width - 1)]
-        |> List.fold (fun lows' x ->
-            let points =
-                adjacents (x, y)
-                |> Set.map (fun (a, b) -> snd heightmap.[b].[a])
-                
-            if Set.all (fun height -> height > snd heightmap.[y].[x]) points
-            then heightmap.[y].[x] :: lows'
-            else lows') lows) []
+    [0 .. (width - 1)]
+    |> List.fold (fun lows x ->
+        [0 ..  (height - 1)]
+        |> List.fold (fun lows' y ->
+            let points = adjacents (x, y) |> Set.map (fun (a, b) -> map.[b].[a])
+            if Set.all (fun h -> h > map.[y].[x]) points then (x, y) :: lows' else lows') lows) []
     
 let rec basin (area: Set<int * int>) =
     let area' =
@@ -38,22 +28,17 @@ let rec basin (area: Set<int * int>) =
         |> Set.map adjacents
         |> Set.unionMany
         |> (><) Set.difference area
-        |> Set.filter (fun (x, y) ->
-            snd heightmap.[y].[x] < 9 &&
-            adjacents (x, y)
-            |> Set.intersect area
-            |> Set.filter (fun (a, b) -> snd heightmap.[b].[a] <= snd heightmap.[y].[x])
-            |> Set.count >= 1)
+        |> Set.filter (fun (x, y) -> map.[y].[x] < 9)
         |> Set.union area
     
     if Set.difference area' area = Set.empty then area' else basin area'      
 
 let part1 =
-    lows |> List.sumBy (fun (_, h) -> h + 1)
+    lows |> List.sumBy (fun (x, y) -> map.[y].[x] + 1)
 
 let part2 =
     lows
-    |> Seq.map (fun (point, _) -> Set.add point Set.empty)
+    |> Seq.map (fun point -> Set.add point Set.empty)
     |> Seq.map (basin >> Set.count)
     |> Seq.sortDescending
     |> Seq.take 3
