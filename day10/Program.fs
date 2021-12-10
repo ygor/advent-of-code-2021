02ad2d359abbebc04c3d06b2f5433fff49690037
Ygor Geurts <ygor.geurts@gmail.com>
2021-12-11 00:55:29 +0100

    simplify

--- day10/Program.fs
+++ day10/Program.fs
@@ -1,49 +1,35 @@
 ﻿open System.IO
-
-type Chunk =
-    | Leaf of char
-    | Node of char * Chunk list
-
-type Line =
-    | Incomplete of char list
-    | Corrupt of char
-    | Chunks of Chunk list
     
 let lines =
     File.ReadAllLines("input.txt") |> Seq.map (List.ofSeq)
 
 let map = [('(', ')'); ('<', '>'); ('{', '}'); ('[', ']')] |> Map.ofList    
 
-let rec parse (chunks: Chunk list) (opens: char list) (line: char list) =
+let rec parse (opens: char list) (line: char list) =
     match line with
-    | [] -> if List.length opens = 0 then Chunks chunks else Incomplete opens
+    | [] -> Result.Ok opens
     | char :: tail ->
-        if Seq.contains char "([<{" then parse chunks (char :: opens) tail
-        elif List.length opens > 0 && map.[List.head opens] = char then
-            let chunk =
-                match List.head chunks with
-                | Leaf o -> Node (o, [Leaf (List.head opens)])
-                | Node (o, xs) -> Node (o, (Leaf (List.head opens)) :: xs) 
-            parse (List.updateAt 0 chunk chunks) (List.tail opens) tail
-        else Corrupt char
+        if Seq.contains char "([<{" then parse (char :: opens) tail
+        elif List.length opens > 0 && map.[List.head opens] = char then parse (List.tail opens) tail
+        else Result.Error char
 
 let part1 =
     let points = [(')', 3); ('>', 25137); ('}', 1197); (']', 57)] |> Map.ofList
 
     lines
-    |> Seq.map (parse [Leaf '.'] [])
+    |> Seq.map (parse [])
     |> Seq.sumBy (fun line ->
         match line with
-        | Corrupt c -> points.[c]
+        | Error c -> points.[c]
         | _ -> 0 )
     
 let part2 =
     let points = [(')', bigint 1); ('>', bigint 4); ('}', bigint 3); (']', bigint 2)] |> Map.ofList
-
+    
     lines
     |> Seq.map (fun line ->
-        match parse [Leaf '.'] [] line with
-        | Incomplete cs -> List.fold (fun score c -> score * (bigint 5) + points.[map.[c]]) (bigint 0) cs
+        match parse [] line with
+        | Ok cs -> List.fold (fun score c -> score * (bigint 5) + points.[map.[c]]) (bigint 0) cs
         | _ -> bigint 0 )
     |> Seq.filter ((<) (bigint 0))
     |> Seq.sort
