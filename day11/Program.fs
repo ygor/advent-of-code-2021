52458bced3330169b62cb8b6c1e59cea6a3880f7
Ygor Geurts <Ygor.Geurts@rtl.nl>
2021-12-11 20:56:36 +0100

    day11 part1

--- day11/Program.fs
+++ day11/Program.fs
@@ -1,38 +1,35 @@
 ﻿open System.IO
 open day11.Extensions
 
-let input = File.ReadAllLines("input.txt") 
+let input = File.ReadAllLines("input.txt")
+
 let map =
     input
-    |> Seq.mapi (fun y line -> line |> Seq.mapi (fun x value -> (x, y), ((string >> int) value, false)))
+    |> Seq.mapi (fun y -> Seq.mapi (fun x value -> (x, y), ((string >> int) value, false)))
     |> Seq.concat
     |> Map.ofSeq
 
-let height, width = Seq.length input, Seq.length input.[0]
-
-let adjacents (x,y) =
+let adjacents (x, y) =
     List.allPairs [-1;0;1] [-1;0;1]
     |> List.map (fun (dx, dy) -> (x + dx, y + dy))
-    |> List.filter (fun (x, y) -> x >= 0 && x < width && y >= 0 && y < height)
-    |> List.except [(x, y)]
+    |> List.filter (fun (x, y) -> x >= 0 && x < (Seq.length input.[0]) && y >= 0 && y < (Seq.length input))
 
-let rec flash (map: Map<int * int, int * bool>) =
+let rec flash map =
     let map' =
-        List.foldn width (fun map x ->
-            List.foldn height (fun (map: Map<int * int, int * bool>) y ->
-                if fst map.[x, y] > 9 then
-                    let map' = Map.add (x, y) (0, true) map
-                    adjacents (x, y)
-                    |> List.fold (fun map'' (a, b) ->
-                        if not (snd map''.[a, b])
-                        then Map.add (a, b) (fst map''.[a, b] + 1, snd map''.[a, b]) map''
-                        else map'') map'
-                else map) map) map
+        Map.keys map
+        |> Seq.fold (fun (map: Map<(int * int),(int * bool)>) (x, y) ->
+            if fst map.[x, y] > 9 then
+                let map' = Map.add (x, y) (0, true) map
+                adjacents (x, y)
+                |> List.fold (fun map' (a, b) ->
+                    let (energy, flashed) = map'.[a, b]
+                    if not flashed then Map.add (a, b) (energy + 1, flashed) map' else map') map'
+            else map) map
     if Map.exists (fun _ (energy, flashed) -> energy > 9 && not flashed) map' then flash map' else map'
     
 let step map =
     map
-    |> Map.map (fun _ value -> fst value + 1, false)
+    |> Map.map (fun _ (energy, _) -> energy + 1, false)
     |> flash
     
 let part1 steps =
