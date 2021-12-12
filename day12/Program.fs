24986de9bb4975ff3d568c1b9907bf9e1d27610f
Ygor Geurts <Ygor.Geurts@rtl.nl>
2021-12-12 11:44:55 +0100

    day12 formatting

--- day12/Program.fs
+++ day12/Program.fs
@@ -8,28 +8,32 @@ let connect (a, b) (caves: Map<string, Set<string>>) =
 let caves =
     File.ReadAllLines("input.txt")
     |> Seq.map (String.split "-")
-    |> Seq.fold (fun caves line ->
-        caves |> connect (line.[0], line.[1]) |> connect (line.[1], line.[0])) Map.empty
+    |> Seq.fold
+        (fun caves line ->
+            caves
+            |> connect (line.[0], line.[1])
+            |> connect (line.[1], line.[0]))
+        Map.empty
     |> Map.map (fun _ -> Set.toList)
-    
-let isSmall (cave: string) =
-    cave.ToUpper() <> cave
-    
+
+let isSmall (cave: string) = cave.ToUpper() <> cave
+
 let rec visit (paths: string list list) =
     paths
-    |> List.map (fun path ->
-        if (List.head path = "end") then [[path]]
-        else
-            caves.[List.head path]
-            |> List.map (fun next ->
-                if (List.contains next path && isSmall next) then [path] else visit [next :: path]))
+    |> List.map
+        (fun path ->
+            if (List.head path = "end") then [ [ path ] ]
+            else
+                caves.[List.head path]
+                |> List.map (fun next ->
+                    if (List.contains next path && isSmall next) then [ path ] else visit [ next :: path ]))
     |> (List.concat >> List.concat)
 
 let part1 =
-    visit [["start"]]
+    visit [ [ "start" ] ]
     |> List.filter (fun path -> List.head path = "end")
     |> List.length
-    
+
 [<EntryPoint>]
 let main _ =
     printfn $"Part 1: %A{part1}"
