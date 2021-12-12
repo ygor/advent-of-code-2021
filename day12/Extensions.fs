51c87aeffc30c700e1116aab26cb2a5df5c890bf
Ygor Geurts <Ygor.Geurts@rtl.nl>
2021-12-12 23:28:41 +0100

    day 12 simplify more

--- day12/Extensions.fs
+++ day12/Extensions.fs
@@ -1,17 +1,12 @@
 module day12.Extensions
 
-module String =
-    let split (sep: string) (input: string) = input.Split(sep) |> List.ofArray
-    
+module String =    
     let split2 (sep: string) (input: string) =
         input.Split(sep)
         |> List.ofArray
         |> (fun list -> (list.[0], list.[1]))
 
 module List =
-    let lengthBy predicate list =
-        list |> List.filter predicate |> List.length
-        
     let flatMap mapping list =
         list
         |> List.map mapping
