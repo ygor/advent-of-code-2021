31e838f343257b1737e3257c69c189376fed1320
Ygor Geurts <ygor.geurts@gmail.com>
2021-12-13 20:21:19 +0100

    simplify

--- day13/Extensions.fs
+++ day13/Extensions.fs
@@ -6,14 +6,3 @@ module String =
         |> List.ofArray
 
     let substring start (string: string) = string.Substring(start)
-        
-module List =
-    let flatMap mapping list =
-        list
-        |> List.map mapping
-        |> List.concat
-        
-    let groupMap projection mapping list =
-        list
-        |> List.groupBy projection
-        |> List.map (fun (key, values) -> key, List.map mapping values)
