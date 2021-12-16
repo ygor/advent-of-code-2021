f10924c859b7012fc304383b9ad817fa46405477
Ygor Geurts <Ygor.Geurts@rtl.nl>
2021-12-16 16:51:37 +0100

    day 15 wip

--- day15/Program.fs
+++ day15/Program.fs
@@ -6,8 +6,8 @@ let risks =
     |> Seq.mapi (fun y -> Seq.mapi (fun x value -> (x, y), ((string >> int) value)))
     |> (Seq.concat >> Map.ofSeq)
 
-let dimensions map =
-    Map.keys map |> Seq.maxBy fst |> fst, Map.keys map |> Seq.maxBy snd |> snd    
+let dimensions (map: Map<int * int, int>) =
+    (map.Keys |> Seq.maxBy fst |> fst) + 1, (map.Keys |> Seq.maxBy snd |> snd) + 1    
 
 let tiling (map: Map<int * int, int>) =
     let width, height = dimensions map
@@ -19,29 +19,26 @@ let tiling (map: Map<int * int, int>) =
 let adjacents (x, y) =
     [(0, 1); (1, 0); (-1, 0); (0, -1)] |> List.map (fun (dx, dy) -> x + dx, y + dy)
 
+let width, height = dimensions risks
+
 let rec findPath current (visited: Set<int * int>) (risks: Map<int * int, int>) (map: Map<int * int, int>) =
-    let width, height = dimensions risks
     let unvisited = adjacents current |> List.filter (fun p -> risks.Keys.Contains p && not(visited.Contains p))
-    let map'' =
+    let map' =
         unvisited
         |> List.fold (fun (map: Map<int * int, int>) a ->
             let value = map.[current] + risks.[a]
             map.Add (a, if map.ContainsKey a && map.[a] < value then map.[a] else value)) map
 
     if current = (width - 1, height - 1)
-    then map''.[width - 1, height - 1]
-    else
-        let next = unvisited |> Seq.min
-        findPath next (visited.Add current) risks map''
+    then map'.[width - 1, height - 1]
+    else findPath (unvisited |> Seq.min) (visited.Add current) risks map'
     
-let part1 = findPath (0, 0) Set.empty risks (Map.add (0,0) 0 Map.empty)
-    
-let part2 =
-    let risks' = tiling risks
-    findPath (0, 0) Set.empty risks' (Map.add (0,0) 0 Map.empty)
+let part1 = findPath (0, 0) (Set.add (0, 0) Set.empty) risks (Map.add (0,0) 0 Map.empty)
+
+//let part2 = findPath (0, 0) Set.empty (tiling risks) (Map.add (0,0) 0 Map.empty)
     
 [<EntryPoint>]
 let main _ =
     printfn $"Part 1: %i{part1}"
-    printfn $"Part 2: %i{part2}"
+//    printfn $"Part 2: %i{part2}"
     0
